7ca8a49b3e089371b7f3f68f606f862d22a2f4c8
Bob Wassermann <bob@wassermann.nl>
2015-01-19 22:06:14 +0100

    including images in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -252,7 +252,8 @@ module.exports = function (grunt) {
             // Like Jekyll, exclude files & folders prefixed with an underscore.
             '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
-            '_bower_components/jquery/dist/jquery.min.js'
+            '_bower_components/jquery/dist/jquery.min.js',
+            'img/_posts/**/*'
             //'favicon.ico',
             //'apple-touch*.png'
           ],
