50ee8b69d4209ed3c69f8d8ef7e220d625460b53
Bob Wassermann <bob@wassermann.nl>
2015-01-19 22:04:17 +0100

    pulled some features out, clean ups

--- Gruntfile.js
+++ Gruntfile.js
@@ -250,9 +250,9 @@ module.exports = function (grunt) {
             'img/**/*',
             'fonts/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
-            '!**/_*{,/**}'
+            '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
-            //'_bower_components/jquery/jquery.js',
+            '_bower_components/jquery/dist/jquery.min.js'
             //'favicon.ico',
             //'apple-touch*.png'
           ],
---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+/*jshint undef:false */
+function calls(){
+  
+  $('.bucket.all').each(function(){
+    var elemCount = $(this).children().size();
+    if(elemCount === 1) {$(this).children().addClass('first');}
+    if(elemCount < 4) {
+     $(this).children().css({'width' : $(window).width() / elemCount, 'height' : $(window).height()});
+    } else {
+     $(this).children().css({'width' : $(window).width() / elemCount*2, 'height' : $(window).height()/2});
+    }
+  });  
+}
+
+$(document).ready(calls);
+$(window).resize(calls);
+
