64dfb98167e3adfa20bb65465c12f2ec79a63cc2
Bob Wassermann <bob@wassermann.nl>
2015-01-27 01:56:29 +0100

    design fixes

--- app/js/main.js
+++ app/js/main.js
@@ -13,9 +13,18 @@ function calls(){
   });
 
 
-  var bg = $('.bucket .card .image').eq(0).css('background-image');
-  var gradientRed = 'linear-gradient(#FF5E3A, #FF2A68)';
-  $('.bucket .card .image').eq(0).css('background-image', 'url( '+bg.slice(4, -1)+', linear-gradient(#FF5E3A, #FF2A68))')
+  var bg = $('.bucket .card .image').eq(0).css('background-image').slice(4, -1);
+  
+  var gradientRed     = 'linear-gradient(#FF5E3A, #FF2A68)';
+  var gradientOrange  = 'linear-gradient(#FF9500, #FF5E3A)';
+  var gradientYellow  = 'linear-gradient(#FFDB4C, #FFCD02)';
+  var gradientGreen   = 'linear-gradient(#87FC70, #87FC70)';
+  var gradientLBlue   = 'linear-gradient(#52EDC7, #52EDC7)';
+
+    $('.bucket .card .image').eq(0).css({'background-image' : 'url('+bg+'), '+gradientRed});
+    $('.bucket .card .image').eq(1).css({'background-image' : 'url('+bg+'), '+gradientLBlue});
+    $('.bucket .card .image').eq(2).css({'background-image' : 'url('+bg+'), '+gradientYellow});
+    $('.bucket .card .image').eq(3).css({'background-image' : 'url('+bg+'), '+gradientGreen});
 }
 
 // Nav
